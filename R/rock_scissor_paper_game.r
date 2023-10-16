dd4a9644c946dd8a4ea57b91b508fe9a7596e46c
Natakorn Laddaklom <147310110+Natakorn-168006@users.noreply.github.com>
2023-10-16 15:12:14 +0700

    Create rock_scissor_paper_game.r

---
+++ R/rock_scissor_paper_game.r
@@ -0,0 +1,50 @@
+
+
+play_game = function(){
+  
+  option = c('rock','paper','scissors')
+  computer_score = 0
+  player_score = 0
+  
+  while(T){
+    
+    player_choice = readline('Your choice:')
+    player_choice = tolower(player_choice)
+    computer_choice = sample(option,1)
+    
+    if (player_choice == 'quit'){
+      print('Goodbye thanks for playing')
+      break
+    } else if (!(player_choice %in% option)){
+      print('Invalid choice, Try again')
+    } else if (player_choice == computer_choice){
+      print("It's a tie")
+    } else if ((player_choice == 'rock' & computer_choice == 'scissors') ||
+              (player_choice == 'paper' & computer_choice == 'rock') ||
+              (player_choice == 'scissors' & computer_choice == 'paper')){
+                
+      print('You win!!!')
+      player_score = player_score+1
+    } else {
+      print('Computer win!!!')
+      computer_score = computer_score+1
+    }
+  }
+  cat('Summary score\n')
+  cat('Player score:',player_score,'\n')
+  cat('Computer score:',computer_score)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
